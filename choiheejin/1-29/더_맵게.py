780b1b540509e20786472bfb804a41ce01f7103e
Hee-jin506 <59636609+Hee-jin506@users.noreply.github.com>
2022-01-27 11:38:45 +0900

    Create 더_맵게.py

---
+++ choiheejin/1-29/더_맵게.py
@@ -0,0 +1,16 @@
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    count = 0
+    while scoville:
+        a = heapq.heappop(scoville)
+        if a >= K:
+            return count
+        if len(scoville) == 0:
+            return -1
+        b = heapq.heappop(scoville)
+        heapq.heappush(scoville, a + b * 2)
+        count += 1
+    
+    return count
