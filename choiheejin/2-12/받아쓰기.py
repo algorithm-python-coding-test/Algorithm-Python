fe0c613b6d17f33028ceafc9be490b505ddf29eb
Hee-jin506 <59636609+Hee-jin506@users.noreply.github.com>
2022-02-12 17:43:04 +0900

    Update 받아쓰기.py

--- choiheejin/2-12/받아쓰기.py
+++ choiheejin/2-12/받아쓰기.py
@@ -3,15 +3,8 @@
 # 비교 테이블 n * m  <= 10,000,000
 
 # 문자 비교
-def check(a, b):
-    if a == b:
-        return True
-    elif a == 'i':
-        if b == 'j' or b == 'l':
-            return True
-    elif a == 'v' and b =='w':
-        return True
-    return False
+def check(i, j):
+    return n[i-1]==m[j-1] or (n[i-1]=='i' and m[j-1] in ['i','j','l']) or (n[i-1]=='v' and m[j-1] in ['v','w'])
 
 nlength, mlength = map(int, input().split())
 n = input()
@@ -23,11 +16,9 @@ LCS = [[i for i in range(mlength + 1)]] + [[i + 1] + [0] * (mlength) for i in ra
 # (1,1) 부터 초기화
 for i in range(1, nlength + 1):
     for j in range(1, mlength + 1):
-        if check(n[i-1], m[j-1]):
+        if check(i, j):
             LCS[i][j] = LCS[i-1][j-1]
         else:
-            LCS[i][j] = min(LCS[i][j - 1], LCS[i - 1][j - 1]) + 1
+            LCS[i][j] = min(LCS[i][j - 1], LCS[i - 1][j - 1], LCS[i-1][j]) + 1
 
 print(LCS[nlength][mlength])
-
-# 시간초과
