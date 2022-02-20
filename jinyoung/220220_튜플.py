9f4fc1536db3a13775b6d41520932ca986afe968
Jinyoung Kim <43057435+jinyoung9305@users.noreply.github.com>
2022-02-20 13:48:25 +0900

    Update 220220_튜플.py

--- jinyoung/220220_튜플.py
+++ jinyoung/220220_튜플.py
@@ -1,3 +1,8 @@
+###개인풀이
+
+###시간 내 통과
+###최소 0.02,최대 24.28ms
+
 def solution(s):
     answer =[]
     #'},{를 기준으로 나눈 후 숫자로 분리하여 리스트의 길이 순으로 정렬
@@ -7,6 +12,7 @@ def solution(s):
     s1 = s[2:-2].split('},{')
     s2 = [i.split(',') for i in s1]
     s2.sort(key=lambda x : len(x))
+    
     #각 리스트의 차집합 요소를 더해가면서 리스트 완성
     for k in s2:
         num = set(k).difference(set(answer))
