6219b4c3e5c40b6471c9a65e64bcfef6f12dd938
Jinyoung Kim <43057435+jinyoung9305@users.noreply.github.com>
2022-02-20 14:34:43 +0900

    Update 220220_징검다리 건너기.py

--- jinyoung/220220_징검다리 건너기.py
+++ jinyoung/220220_징검다리 건너기.py
@@ -1,11 +1,11 @@
-#개인 풀이
+###개인 풀이
 
 ###통과 못함
 
 
 def solution(stones, k):
     
-    ##zero_index는 0의 시작 인덱스와 길이를 담은 딕셔너리
+    ##zero_index는 0의 시작 인덱스와 연속되는 0의 길이를 담은 딕셔너리
     ##ex [1, 3, 4, 2, 1, 0, 3, 1, 4, 0] 인 경우 zero_index = {5: 1, 9: 1}
     
     zero_index = {}
@@ -18,27 +18,30 @@ def solution(stones, k):
         stones = [i-1 for i in stones]
         num+=1
 
-        #0값이 추가되었을 경우 딕셔너리에 더해주기
+        #stones에 0값이 추가되었을 경우 zero_index 딕셔너리에 더해주기
         for p in range(len(stones)):
             if stones[p]==0:
                 if p in zero_index:
                     continue
                 else:
                     zero_index[p]=1
-        print(zero_index)
+        
+        #해당 인덱스(idx)의 오른쪽(idx+1)에 0이 존재하는 경우 
+        #해당 인덱스(idx)의 값을 오른쪽(idx+1)의 0의 길이만큼 더해주고 오른쪽(idx+1)의 값은 -1처리 
+        
         for idx in zero_index:
+            #인덱스의 값이 -1일 경우 넘어가기
             if zero_index[idx] ==-1:
                 continue
-            #인덱스의 오른쪽 값(idx+1)에 0이 존재하는 경우 오른쪽 길이 값을 더해주고 idx+1은 -1처리
-            # ex {5: 1, 9: 1, 0: 1, 4: 1, 7: 1} -> {5: -1, 9: 1, 0: 1, 4: 2, 7: 1}
-
+                
+            # ex [1, 3, 4, 2, 1, 0, 3, 1, 4, 0] -> [0, 2, 3, 1, 0, 0, 2, 0, 3, 0]
+            # ex {5: 1, 9: 1} -> {5: 1, 9: 1, 0: 1, 4: 1, 7: 1} -> {5: -1, 9: 1, 0: 1, 4: 2, 7: 1}
             if (idx!=len(stones)-1) and (idx+1 in zero_index) and (zero_index[idx+1]!=-1):
                 zero_index[idx]+=zero_index[idx+1]
                 zero_index[idx+1] =-1
                 
                     
         mx = max(list(zero_index.values()))
-        print(zero_index)
     return num-1
 
 
