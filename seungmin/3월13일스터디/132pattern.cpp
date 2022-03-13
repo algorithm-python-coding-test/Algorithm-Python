22cc5bdd36da4d6fef7ff1c089ce75931f4fcdf8
SeunminKim <qpemfh5580@naver.com>
2022-03-13 12:14:14 +0900

    스택 답

--- seungmin/3월13일스터디/132pattern.cpp
+++ seungmin/3월13일스터디/132pattern.cpp
@@ -8,7 +8,7 @@
 // 
 //  { -2,1,2,-2,1,2 } 에서 안됨 중복되는 값이 있으면 앞에서부터 순회하는 방식이 먹히지 않음!
 
-
+/*  
 #include <string>
 #include <vector>
 #include <iostream>
@@ -91,4 +91,52 @@ int main() {
     cout << b << '\n';
 
     return 0;
-}
+} 
+*/
+
+// 스택을 활용한 답
+
+class Solution {
+public:
+    //for each i, find the prefix min(nums[0:i])
+    //for each i, fint the cloest larger element nums[j] (j<i)
+    //  if prefix_min[j] < nums[i], then return true
+    bool find132pattern(vector<int>& nums) {
+        int const n = nums.size();
+        vector<int> prefix_min(n);
+        prefix_min[0] = INT_MAX;
+        for (int i = 1; i < n; i++) {
+            // i번째보다 이전 값들 중 가장 작은 값을 prefix_min[i]에 저장
+            prefix_min[i] = min(prefix_min[i - 1], nums[i - 1]);
+        }
+        // index를 저장하는 stack
+        stack<int> stk; //store index, nums[i] decreasing
+        stk.push(0);
+        for (int i = 1; i < n; i++) {
+            // stack에 nums[i]보다 큰 값들만 두고 나머지는 pop하기
+            // stack에 있는 값들은 i보다 앞에 있는 index들
+            while (!stk.empty() && nums[stk.top()] <= nums[i]) {
+                stk.pop();
+                // stk.top()만 확인해도 되는 이유는
+                // 앞에서부터 확인하여 nums[stk.top()] > nums[i]를 만족하는 nums[i]를 
+                // stk에 push하기 때문에 stk.top()의 값이 stk에 들어있는 모든 값들 중 가장 작다 (min_heap을 안써도 되는 이유)
+            }
+            // stk에 있는 모든 j값은
+            // j < i 와 nums[i] < nums[j]를 만족한다
+            if (!stk.empty()) { //exist a larger element nums[j]
+                // stk.top()의 prefix_min이 nums[i]보다 작다면
+                // 
+                // prefix_min[stk.top()] < nums[i] < nums[stk.top()] 와 
+                // prefix_min[stk.top()]의 index < stk.top() < i 를 만족하므로 true
+                if (prefix_min[stk.top()] < nums[i]) {
+                    return true;
+                }
+                // stk의 top만 확인해도 되는 이유
+                // stk에 push되는 조건은 stk에 이전에 먼저 들어온 모든 값들의 prefix_min보다 작거나 같아야 한다
+                // 그래서 stk.top()의 prefix_min이 stk의 모든 prefix_min 중 가장 작은 값이 된다
+            }
+            stk.push(i);
+        }
+        return false;
+    }
+};
